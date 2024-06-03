c2a28a83da5fe70aa57dc1256a1f1e4c45e6442a
malakehab2003 <lokaehab2003@gmail.com>
2024-06-03 17:58:41 +0300

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" create auth class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ manage the API authentication """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """ authorization header """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user """
+        return None
