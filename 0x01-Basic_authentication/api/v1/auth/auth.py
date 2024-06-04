0e34ad973c0164fe8acad2d04428c8c3baa70d44
malakehab2003 <lokaehab2003@gmail.com>
2024-06-04 14:42:20 +0300

    task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ authorization header """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """
         return None
