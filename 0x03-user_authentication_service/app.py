62603c27fc0f4061f8daa7db67ef7955a33305a8
malakehab2003 <lokaehab2003@gmail.com>
2024-06-13 20:12:27 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,15 +77,23 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": token})
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
-    ''' update password '''
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+
+    Return:
+        - The user's password updated payload.
+    """
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
+    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-    except Exception:
+        is_password_changed = True
+    except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"})
 
