b100571ce1e74dd09a18c2eaef64191454e9b4b4
malakehab2003 <lokaehab2003@gmail.com>
2024-06-13 19:45:26 +0300

    task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except Exception:
-            raise ValueError
+            raise ValueError()
         new_pass = _hash_password(password)
         self._db.update_user(
             user.id,
