677b67e483cabb3456562dcc30e632784d5ac88a
malakehab2003 <lokaehab2003@gmail.com>
2024-06-16 16:00:34 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,12 +26,17 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields:List[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Format the record by filtering sensitive fields """
         original_message = super().format(record)
-        filtered_message = filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        filtered_message = filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR
+        )
         return filtered_message
