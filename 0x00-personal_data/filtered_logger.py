7abd60497680166c6b19ff947c09c6aed52a5158
malakehab2003 <lokaehab2003@gmail.com>
2024-06-16 15:58:43 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ create filter_datum function """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(
@@ -15,3 +16,22 @@ def filter_datum(
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields:List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the record by filtering sensitive fields """
+        original_message = super().format(record)
+        filtered_message = filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return filtered_message
