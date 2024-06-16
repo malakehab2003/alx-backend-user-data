ff9cfe03978b4a1d522073227fa46b743dfa505a
malakehab2003 <lokaehab2003@gmail.com>
2024-06-17 02:31:39 +0300

    task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,9 @@ if auth_type is not None:
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+if auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" create class SessionAuth """
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ session auth class """
+    pass
