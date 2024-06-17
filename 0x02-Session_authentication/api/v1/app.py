0aeeb42e2ca23e8db50710961fc2517a931189ce
malakehab2003 <lokaehab2003@gmail.com>
2024-06-17 16:24:46 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -66,11 +66,6 @@ def authentication():
             if auth.current_user(request) is None:
                 abort(403)
             request.current_user = auth.current_user(request)
-            if (
-                auth.authorization_header(request) is None
-                and auth.session_cookie(request) is None
-            ):
-                abort(401)
 
 
 if __name__ == "__main__":
